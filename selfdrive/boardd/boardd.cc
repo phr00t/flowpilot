d7c821b429222922654f0b176813d03b45fa0905
Phr00t <phr00t@gmail.com>
2023-07-17 17:37:21 -0400

    includes

--- selfdrive/boardd/boardd.cc
+++ selfdrive/boardd/boardd.cc
@@ -23,13 +23,13 @@
 
 #include "cereal/gen/cpp/car.capnp.h"
 #include "cereal/messaging/messaging.h"
-#include "selfdrive/common/params.h"
-#include "selfdrive/common/swaglog.h"
-#include "selfdrive/common/timing.h"
-#include "selfdrive/common/util.h"
-#include "selfdrive/hardware/hw.h"
+#include "common/params.h"
+#include "common/swaglog.h"
+#include "common/timing.h"
+#include "common/util.h"
+#include "hardware/hw.h"
 
-#include "selfdrive/boardd/pigeon.h"
+#include "boardd/pigeon.h"
 
 // -- Multi-panda conventions --
 // Ordering:
