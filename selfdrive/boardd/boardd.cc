cdb9d7a85df8bfef902f7567acea7e397d754231
Phr00t <phr00t@gmail.com>
2023-07-24 13:24:53 -0400

    test

--- selfdrive/boardd/boardd.cc
+++ selfdrive/boardd/boardd.cc
@@ -651,10 +651,13 @@ void boardd_main_thread(std::vector<std::string> serials) {
     // connect to all
     serials = Panda::list();
 
-    // exit if no pandas are connected
+    // send dummy info if no pandas are connected
     if (serials.size() == 0) {
-      LOGW("no pandas found, exiting");
-      return;
+        while(true) {
+          send_dummy_panda_state(&pm);
+          send_empty_peripheral_state(&pm);
+          util::sleep_for(500);
+        }
     }
   }
 
