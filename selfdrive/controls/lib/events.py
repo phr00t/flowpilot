0ca989895743801cd667e4e79aeb97906fe116b7
Phr00t <phr00t@gmail.com>
2023-08-19 14:41:39 -0400

    get rid of this annoying beep

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -549,7 +549,7 @@ EVENTS: Dict[int, Dict[str, Union[Alert, AlertCallbackType]]] = {
       "Take Control",
       "Turn Exceeds Steering Limit",
       AlertStatus.userPrompt, AlertSize.mid,
-      Priority.LOW, VisualAlert.steerRequired, AudibleAlert.promptRepeat, 1.),
+      Priority.LOW, VisualAlert.steerRequired, AudibleAlert.none, 1.),
   },
 
   # Thrown when the fan is driven at >50% but is not rotating
