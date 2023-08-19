35cb3367c22325eee35374994b6dab5ac253ffd4
Phr00t <phr00t@gmail.com>
2023-08-19 12:36:40 -0400

    force nosensor mode

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -34,7 +34,7 @@ LANE_DEPARTURE_THRESHOLD = 0.1
 
 REPLAY = "REPLAY" in os.environ
 SIMULATION = "SIMULATION" in os.environ
-NOSENSOR = "NOSENSOR" in os.environ
+NOSENSOR = True #"NOSENSOR" in os.environ
 IGNORE_PROCESSES = {"loggerd", "logmessaged", "gradled", "uploader", "deleter", "proclogd"} # TODO
 
 ThermalStatus = log.DeviceState.ThermalStatus
