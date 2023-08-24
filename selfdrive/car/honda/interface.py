1d94dccf25bfe30d5eb841e5756280ae4f893135
Phr00t <phr00t@gmail.com>
2023-08-23 20:20:32 -0400

    brakeUnavailable is apparently not available

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -326,8 +326,8 @@ class CarInterface(CarInterfaceBase):
 
     # events
     events = self.create_common_events(ret, pcm_enable=False)
-    if self.CS.brake_error:
-      events.add(EventName.brakeUnavailable)
+    #if self.CS.brake_error:
+    #  events.add(EventName.brakeUnavailable)
 
     if self.CP.pcmCruise and ret.vEgo < self.CP.minEnableSpeed:
       events.add(EventName.belowEngageSpeed)
