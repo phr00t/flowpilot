e231af9e0a88b0d378449c2d09bc8dbd9fa91898
Phr00t <phr00t@gmail.com>
2023-08-27 10:51:58 -0400

    more advanced camera intrinsic management

--- common/java/ai.flow.common/transformations/Camera.java
+++ common/java/ai.flow.common/transformations/Camera.java
@@ -5,29 +5,32 @@ import org.nd4j.linalg.factory.Nd4j;
 
 public class Camera {
 
-    // lots of important stuff calculated from this (Camera #2)
+    // lots of important stuff calculated from this
+    // current set to LG G8 wide camera
+    public static final float
+        FocalX = 2180f, //1394.7081f,
+        FocalY = 2180f, //1394.7616f,
+        CenterX = 952.62915f,
+        CenterY = 517.53534f;
+
+    // everything autocalculated below
+    public static final float eon_wide_focal_length = 910.0f; // model expects this for some reason
+    public static final float actual_cam_focal_length = (FocalX + FocalY) * 0.5f;
+    public static final float digital_zoom_apply = actual_cam_focal_length / eon_wide_focal_length;
+    public static final int[] frameSize = new int[]{1920, 1080};
+    public static final float OffsetX = CenterX - (frameSize[0]*0.5f);
+    public static final float OffsetY = CenterY - (frameSize[1]*0.5f);
+
     public static final float[] WideIntrinsics = {
-            1394.7081f, 0.0f, 952.62915f,
-            0.0f,   1394.7616f, 517.53534f,
+            FocalX, 0.0f, frameSize[0] * 0.5f + OffsetX * digital_zoom_apply,
+            0.0f, FocalY, frameSize[1] * 0.5f + OffsetY * digital_zoom_apply,
             0.0f,   0.0f, 1.0f
     };
 
-    // Camera #0 (telephoto)
-    /*public static final float[] WideIntrinsics = {
-            910f,   0.0f, 900f,
-            0.0f,   910f, 514f,
-            0.0f,   0.0f, 1.0f
-    };*/
-
     // everything auto-generated from above
-    public static final float eon_wide_focal_length = 910.0f; // model expects this for some reason
-    public static final float actual_cam_intrinsics = (WideIntrinsics[0] + WideIntrinsics[4]) * 0.5f;
-    public static final float digital_zoom_apply = actual_cam_intrinsics / eon_wide_focal_length;
-
     public static final int CAMERA_TYPE_ROAD = 0;
     public static final int CAMERA_TYPE_WIDE = 1;
     public static final int CAMERA_TYPE_DRIVER = 2;
-    public static final int[] frameSize = new int[]{1920, 1080};
     public static INDArray wide_intrinsics = Nd4j.createFromArray(new float[][]{
             { WideIntrinsics[0],  0.0f,  WideIntrinsics[2]},
             {0.0f,  WideIntrinsics[4],  WideIntrinsics[5]},
@@ -38,4 +41,11 @@ public class Camera {
             {0.0f,  0.0f,  1.0f},
             {1.0f,  0.0f,  0.0f}
     });
+
+    // Camera #0 (telephoto)
+    /*public static final float[] WideIntrinsics = {
+            910f,   0.0f, 900f,
+            0.0f,   910f, 514f,
+            0.0f,   0.0f, 1.0f
+    };*/
 }
--- selfdrive/ui/java/ai.flow.app/OnRoadScreen.java
+++ selfdrive/ui/java/ai.flow.app/OnRoadScreen.java
@@ -68,7 +68,7 @@ import static ai.flow.sensor.messages.MsgFrameBuffer.updateImageBuffer;
 
 public class OnRoadScreen extends ScreenAdapter {
     // avoid GC triggers.
-    static final String VERSION = "0.18";
+    static final String VERSION = "0.19";
     final WorkspaceConfiguration wsConfig = WorkspaceConfiguration.builder()
             .policyAllocation(AllocationPolicy.STRICT)
             .policyLearning(LearningPolicy.FIRST_LOOP)
