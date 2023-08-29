96ba9b5a330ce957774101f9b09713d83eb8b5eb
Phr00t <phr00t@gmail.com>
2023-08-28 22:51:24 -0400

    i want the raw accel data here, don't clamp it so tightly

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -13,8 +13,8 @@ Ecu = car.CarParams.Ecu
 
 
 class CarControllerParams:
-  ACCEL_MIN = -3.5 # m/s
-  ACCEL_MAX = 2.0 # m/s
+  ACCEL_MIN = -50 # m/s
+  ACCEL_MAX = 50 # m/s
 
   def __init__(self, CP):
     self.STEER_DELTA_UP = 3
