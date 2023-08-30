76a825e2f85b2e1acbe653679cd734a948119200
Phr00t <phr00t@gmail.com>
2023-08-29 20:20:50 -0400

    tuning

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -57,7 +57,7 @@ class CarInterface(CarInterfaceBase):
       if 0x38d in fingerprint[0] or 0x38d in fingerprint[2]:
         ret.flags |= HyundaiFlags.USE_FCA.value
 
-    ret.steerActuatorDelay = 0.3  # kona ev delay
+    ret.steerActuatorDelay = 0.33  # kona ev delay
     ret.steerLimitTimer = 0.4
     tire_stiffness_factor = 1.
     CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
@@ -107,7 +107,7 @@ class CarInterface(CarInterfaceBase):
       ret.mass = {CAR.KONA_EV: 1685., CAR.KONA_HEV: 1425., CAR.KONA_EV_2022: 1743.}.get(candidate, 1275.) + STD_CARGO_KG
       ret.wheelbase = 2.6
       ret.steerRatio = 13.42
-      tire_stiffness_factor = 0.85
+      tire_stiffness_factor = 0.385
       #ret.lateralTuning.torque.kf = 0.975
       #ret.lateralTuning.torque.kp = 0.9
     elif candidate in (CAR.IONIQ, CAR.IONIQ_EV_LTD, CAR.IONIQ_PHEV_2019, CAR.IONIQ_HEV_2022, CAR.IONIQ_EV_2020, CAR.IONIQ_PHEV):
