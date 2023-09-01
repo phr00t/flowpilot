691013a0621557f3cf54a29e2cfadb04a3e6e4b7
Phr00t <phr00t@gmail.com>
2023-08-31 23:09:47 -0400

    more realistic clamp

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -65,7 +65,7 @@ class LanePlanner:
 
     # Find current lanewidth
     self.lane_width_certainty.update(l_prob * r_prob)
-    current_lane_width = clamp(abs(self.rll_y[0] - self.lll_y[0]), 2.0, 4.0)
+    current_lane_width = clamp(abs(self.rll_y[0] - self.lll_y[0]), 2.6, 4.0)
     self.lane_width_estimate.update(current_lane_width)
     speed_lane_width = interp(v_ego, [0., 31.], [2.7, 3.7])
     self.lane_width = self.lane_width_certainty.x * self.lane_width_estimate.x + \
