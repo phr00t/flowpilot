a09e3bfcde863ee1f8e8c7c6f4e64ea8fc960f01
Phr00t <phr00t@gmail.com>
2023-09-27 12:30:03 -0400

    back to latest SNPE libraries and NLP model

--- selfdrive/ui/java/ai.flow.app/OnRoadScreen.java
+++ selfdrive/ui/java/ai.flow.app/OnRoadScreen.java
@@ -68,7 +68,7 @@ import static ai.flow.sensor.messages.MsgFrameBuffer.updateImageBuffer;
 
 public class OnRoadScreen extends ScreenAdapter {
     // avoid GC triggers.
-    static final String VERSION = "49";
+    static final String VERSION = "50";
     final WorkspaceConfiguration wsConfig = WorkspaceConfiguration.builder()
             .policyAllocation(AllocationPolicy.STRICT)
             .policyLearning(LearningPolicy.FIRST_LOOP)
