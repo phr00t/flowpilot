beff3cefec1118797cf8ec7678427c1e060d1f8d
Phr00t <phr00t@gmail.com>
2023-10-05 23:58:45 -0400

    bump android app version

--- selfdrive/ui/java/ai.flow.app/OnRoadScreen.java
+++ selfdrive/ui/java/ai.flow.app/OnRoadScreen.java
@@ -68,7 +68,7 @@ import static ai.flow.sensor.messages.MsgFrameBuffer.updateImageBuffer;
 
 public class OnRoadScreen extends ScreenAdapter {
     // avoid GC triggers.
-    static final String VERSION = "52";
+    static final String VERSION = "53";
     final WorkspaceConfiguration wsConfig = WorkspaceConfiguration.builder()
             .policyAllocation(AllocationPolicy.STRICT)
             .policyLearning(LearningPolicy.FIRST_LOOP)
