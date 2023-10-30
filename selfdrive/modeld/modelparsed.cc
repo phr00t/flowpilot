03c0fc7b6d92fbdebdcb48682ae7b63e881eb3a7
Phr00t <phr00t@gmail.com>
2023-10-30 13:29:52 -0400

    WIP external thneed runnner, hooking up modelparsed

--- selfdrive/modeld/modelparsed.cc
+++ selfdrive/modeld/modelparsed.cc
@@ -1,26 +1,120 @@
 #include <assert.h>
 #include <sched.h>
+#include <iostream>
+#include <cstring>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
 
 #include "cereal/messaging/messaging.h"
 #include "selfdrive/modeld/models/driving.h"
 #include "common/util.h"
 
+#define USE_SOCKETS
+
+// function that reads data from an input stream until a given number of bytes are read
+// returns true if successful, false otherwise
+bool readFully(int sock, char *buffer, int length) {
+    int bytesRead = 0; // number of bytes read so far
+    while (bytesRead < length) { // while there are more bytes to read
+        // read from the input stream and store in buffer
+        int result = recv (sock, buffer + bytesRead, length - bytesRead, 0);
+        if (result < 0) { // if an error occurred
+	    printf("recv result: %d, errno %d", result, errno);
+            return false; // return false
+        }
+        else if (result == 0) { // if end of stream reached
+	    printf("recv result end of stream");
+            return false; // return false
+        }
+        else { // if some bytes were read
+            bytesRead += result; // update number of bytes read
+        }
+    }
+    return true; // return true when all bytes are read
+}
+
+int getServerSocket(int port) {
+    int server_sock; // server socket descriptor for receiving data from Java application
+    int server_conn_sock; // server socket connection descriptor for receiving data from Java application
+    struct sockaddr_in server_addr; // server address structure for receiving data from Java application
+    socklen_t sin_size; // size of sockaddr_in structure
+    // set the server address and port for receiving data from Java application
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons (port); // server port for receiving data from Java application
+    server_addr.sin_addr.s_addr = INADDR_ANY; // server IP address for receiving data from Java application
+    memset (&(server_addr.sin_zero), 0, 8); // zero the rest of the structure
+    // create a new TCP socket for receiving data from Java application
+    if ((server_sock = socket (AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror ("socket");
+        return -1;
+    }
+
+    // bind the socket to the server address and port for receiving data from Java application
+    if (::bind (server_sock, (struct sockaddr *)&server_addr, sizeof (struct sockaddr)) == -1) {
+        perror ("bind");
+        return -1;
+    }
+
+    // listen for incoming connections on the socket for receiving data from Java application
+    if (listen (server_sock, 1) == -1) {
+        perror ("listen");
+        return -1;
+    }
+
+    cout << "Server listening on port " << port << " for receiving data" << endl;
+
+    // accept an incoming connection on the socket for receiving data from Java application
+    sin_size = sizeof (struct sockaddr_in);
+    if ((server_conn_sock = accept (server_sock, (struct sockaddr *)&client_addr, &sin_size)) == -1) {
+        perror ("accept");
+        return -1;
+    }
+
+    cout << "Server accepted connection from " << inet_ntoa (client_addr.sin_addr) << " for receiving data" << endl;
+	
+	return server_conn_sock;
+}
 
 ExitHandler do_exit;
 float model_raw_preds[NET_OUTPUT_SIZE];
 
 int main(int argc, char **argv) {
-  AlignedBuffer aligned_buf;
 
   PubMaster pm({"modelV2", "cameraOdometry"});
 
+#ifdef USE_SOCKETS
+  int server_socket = -1;
+#else
+  AlignedBuffer aligned_buf;
   std::unique_ptr<Context> context(Context::create());
   std::unique_ptr<SubSocket> subscriber(SubSocket::create(context.get(), "modelRaw"));
   assert(subscriber != NULL);
+#endif
 
   uint32_t last_frame_id = 0;
 
   while (!do_exit) {
+#ifdef USE_SOCKETS
+	if (server_socket == -1)
+		server_socket = getServerSocket(9229);
+	
+	// read the data from the input stream without reading the length first
+	success = readFully(server_socket, (char*)&model_raw_preds[0], NET_OUTPUT_SIZE * 4);
+
+	if (!success) { // if an error occurred or end of stream reached
+		server_socket = -1;
+		sched_yield();
+		continue;
+	}	
+
+    model_publish(pm, 0, 0, 0, 0, model_raw_preds, 0, 0, true);
+    posenet_publish(pm, 0, 0, model_raw_preds, 0, true);
+
+#else
     // this receive should block
     std::unique_ptr<Message> msg(subscriber->receive());
     if (!msg) {
@@ -29,7 +123,6 @@ int main(int argc, char **argv) {
       }
       continue;
     }
-
     capnp::FlatArrayMessageReader cmsg(aligned_buf.align(msg.get()));
     cereal::Event::Reader event = cmsg.getRoot<cereal::Event>();
     auto modelRaw = event.getModelRaw();
@@ -46,6 +139,8 @@ int main(int argc, char **argv) {
     posenet_publish(pm, modelRaw.getFrameId(), vipc_dropped_frames, model_raw_preds, modelRaw.getTimestampEof(), modelRaw.getValid());
 
     last_frame_id = modelRaw.getFrameId();
+#endif
+
   }
   return 0;
 }
--- thneedrunner/thneedapp.cpp
+++ thneedrunner/thneedapp.cpp
@@ -6,6 +6,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sched.h>
 
 #include "selfdrive/modeld/models/driving.h"
 #include "thneedmodel.h"
@@ -54,73 +55,82 @@ bool writeFully(int sock, char *buffer, int length) {
     return true; // return true when all bytes are written
 }
 
-int main () {
-
+int getServerSocket(int port) {
     int server_sock; // server socket descriptor for receiving data from Java application
     int server_conn_sock; // server socket connection descriptor for receiving data from Java application
-    int client_sock; // client socket descriptor for sending data to another C++ application
-
     struct sockaddr_in server_addr; // server address structure for receiving data from Java application
-    struct sockaddr_in client_addr; // client address structure for sending data to another C++ application
-
     socklen_t sin_size; // size of sockaddr_in structure
-
-    bool success; // flag to indicate success or failure
-
+    // set the server address and port for receiving data from Java application
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons (port); // server port for receiving data from Java application
+    server_addr.sin_addr.s_addr = INADDR_ANY; // server IP address for receiving data from Java application
+    memset (&(server_addr.sin_zero), 0, 8); // zero the rest of the structure
     // create a new TCP socket for receiving data from Java application
     if ((server_sock = socket (AF_INET, SOCK_STREAM, 0)) == -1) {
         perror ("socket");
-        exit (1);
+        return -1;
     }
-
-    // set the server address and port for receiving data from Java application
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons (8228); // server port for receiving data from Java application
-    server_addr.sin_addr.s_addr = INADDR_ANY; // server IP address for receiving data from Java application
-    memset (&(server_addr.sin_zero), 0, 8); // zero the rest of the structure
 
     // bind the socket to the server address and port for receiving data from Java application
     if (::bind (server_sock, (struct sockaddr *)&server_addr, sizeof (struct sockaddr)) == -1) {
         perror ("bind");
-        exit (1);
+        return -1;
     }
 
     // listen for incoming connections on the socket for receiving data from Java application
     if (listen (server_sock, 1) == -1) {
         perror ("listen");
-        exit (1);
+        return -1;
     }
 
-    cout << "Server listening on port 8228 for receiving data from Java application" << endl;
+    cout << "Server listening on port " << port << " for receiving data" << endl;
 
     // accept an incoming connection on the socket for receiving data from Java application
     sin_size = sizeof (struct sockaddr_in);
     if ((server_conn_sock = accept (server_sock, (struct sockaddr *)&client_addr, &sin_size)) == -1) {
         perror ("accept");
-        exit (1);
+        return -1;
     }
 
-    cout << "Server accepted connection from " << inet_ntoa (client_addr.sin_addr) << " for receiving data from Java application" << endl;
+    cout << "Server accepted connection from " << inet_ntoa (client_addr.sin_addr) << " for receiving data" << endl;
+	
+	return server_conn_sock;
+}
 
+int getClientSocket(int port) {
+    int client_sock; // client socket descriptor for sending data to another C++ application
+    struct sockaddr_in client_addr; // client address structure for sending data to another C++ application
+	
     // create a new TCP socket for sending data to another C++ application
     if ((client_sock = socket (AF_INET, SOCK_STREAM, 0)) == -1) {
         perror ("socket");
-        exit (1);
+        return -1;
     }
 
     // set the client address and port for sending data to another C++ application
     client_addr.sin_family = AF_INET;
-    client_addr.sin_port = htons (9229); // client port for sending data to another C++ application
+    client_addr.sin_port = htons (port); // client port for sending data to another C++ application
     client_addr.sin_addr.s_addr = inet_addr ("127.0.0.1"); // client IP address for sending data to another C++ application
     memset (&(client_addr.sin_zero), 0, 8); // zero the rest of the structure
 
     // connect to the server address and port for sending data to another C++ application
-    /*if (connect (client_sock, (struct sockaddr *)&client_addr, sizeof (struct sockaddr)) == -1) {
+    if (connect (client_sock, (struct sockaddr *)&client_addr, sizeof (struct sockaddr)) == -1) {
         perror ("connect");
-        exit (1);
-    }*/
+        return -1;
+    }
 
-    cout << "Client connected to " << inet_ntoa (client_addr.sin_addr) << " on port 9229 for sending data to another C++ application" << endl;
+    cout << "Client connected to " << inet_ntoa (client_addr.sin_addr) << " on port " << port << " for sending data to another C++ application" << endl;
+	
+	return client_sock;
+}
+
+int main () {
+
+
+    bool success; // flag to indicate success or failure
+	
+	int server_conn_sock = -1;
+	int client_sock = -1;
 
 	bool do_exit;
 
@@ -152,78 +162,96 @@ int main () {
 	thneed->addInput("traffic_convention", model_input, 8/4);
 	thneed->addInput("nav_features", model_input, 1024/4);
 	thneed->addInput("nav_instructions", model_input, 600/4);
-        thneed->addInput("features_buffer", model_input + StartFeatures, features_len);
-        thneed->addInput("desire", model_input + StartDesire, desire_len);
+	thneed->addInput("features_buffer", model_input + StartFeatures, features_len);
+	thneed->addInput("desire", model_input + StartDesire, desire_len);
 
 	uint32_t last_frame_id = 0;
 	bool inputsSet = false;
 
     // loop until the connection is closed
     while (true) {
-
+		
+		// make sure we have a valid server connection
+		if (server_conn_sock == -1)
+			server_conn_sock = getServerSocket(8228); // get data from Java app
+		
+		// if we don't have a client connection, get that
+		if (client_sock == -1)
+			client_sock = getClientSocket(9229);
+		
+		if (client_sock == -1 || server_conn_sock == -1) {
+			// broken connection to apps, loop again and try
+			sched_yield();
+			continue;
+		}
+		
         // read the data from the input stream without reading the length first
         success = readFully(server_conn_sock, (char*)&model_input[StartInput], total_input_len * 4);
 
         if (!success) { // if an error occurred or end of stream reached
-            break; // break from the loop
+            server_conn_sock = -1;
+			sched_yield();
+			continue;
         }
 
         cout << "Server received data from Java application" << endl;
 
-	int desire = *((int*)&model_input[StartDesireInt]);
-        float *input_imgs = &model_input[StartInput];
-        float *big_input_imgs = &model_input[StartInput + input_imgs_len]; // &model_input[1+input_imgs_len];
+		int desire = *((int*)&model_input[StartDesireInt]);
+		float *input_imgs = &model_input[StartInput];
+		float *big_input_imgs = &model_input[StartInput + input_imgs_len]; // &model_input[1+input_imgs_len];
 
-        // set images from android app
-        if (inputsSet == false) {
-            thneed->addInput("input_imgs", &model_input[StartInput], input_imgs_len);
-	    thneed->addInput("big_input_imgs", &model_input[StartInput + input_imgs_len], input_imgs_len);
-            inputsSet = true;
-        } else {
-            thneed->setInputBuffer("input_imgs", &model_input[StartInput], input_imgs_len);
-	    thneed->setInputBuffer("big_input_imgs", &model_input[StartInput + input_imgs_len], input_imgs_len);
-    	}
+		// set images from android app
+		if (inputsSet == false) {
+			thneed->addInput("input_imgs", &model_input[StartInput], input_imgs_len);
+		thneed->addInput("big_input_imgs", &model_input[StartInput + input_imgs_len], input_imgs_len);
+			inputsSet = true;
+		} else {
+			thneed->setInputBuffer("input_imgs", &model_input[StartInput], input_imgs_len);
+		thneed->setInputBuffer("big_input_imgs", &model_input[StartInput + input_imgs_len], input_imgs_len);
+		}
 
-        cout << "Made inputs, now doing desire..." << endl;
+		cout << "Made inputs, now doing desire..." << endl;
 
-	// handle desire
-	float vec_desire[DESIRE_LEN] = {0};
-	if (desire >= 0 && desire < DESIRE_LEN) {
-	  vec_desire[desire] = 1.0;
-	}
+		// handle desire
+		float vec_desire[DESIRE_LEN] = {0};
+		if (desire >= 0 && desire < DESIRE_LEN) {
+		  vec_desire[desire] = 1.0;
+		}
 
-	memmove(&model_input[StartDesire], &model_input[StartDesire + DESIRE_LEN], sizeof(float) * DESIRE_LEN*HISTORY_BUFFER_LEN);
-	for (int i = 1; i < DESIRE_LEN; i++) {
-	  // Model decides when action is completed
-	  // so desire input is just a pulse triggered on rising edge
-	  if (vec_desire[i] - prev_desire[i] > .99) {
-			model_input[StartDesire + DESIRE_LEN*HISTORY_BUFFER_LEN+i] = vec_desire[i];
-	  } else {
-			model_input[StartDesire + DESIRE_LEN*HISTORY_BUFFER_LEN+i] = 0.0;
-	  }
-	  prev_desire[i] = vec_desire[i];
-	}
+		memmove(&model_input[StartDesire], &model_input[StartDesire + DESIRE_LEN], sizeof(float) * DESIRE_LEN*HISTORY_BUFFER_LEN);
+		for (int i = 1; i < DESIRE_LEN; i++) {
+		  // Model decides when action is completed
+		  // so desire input is just a pulse triggered on rising edge
+		  if (vec_desire[i] - prev_desire[i] > .99) {
+				model_input[StartDesire + DESIRE_LEN*HISTORY_BUFFER_LEN+i] = vec_desire[i];
+		  } else {
+				model_input[StartDesire + DESIRE_LEN*HISTORY_BUFFER_LEN+i] = 0.0;
+		  }
+		  prev_desire[i] = vec_desire[i];
+		}
 
-        cout << "Executing model!" << endl;
+		cout << "Executing model!" << endl;
 
-	thneed->execute();
+		thneed->execute();
 
-        cout << "Handling features..." << endl;
+		cout << "Handling features..." << endl;
 
-	// handle features
-	std::memmove(&model_input[StartFeatures], &model_input[StartFeatures + FEATURE_LEN], sizeof(float) * FEATURE_LEN*(HISTORY_BUFFER_LEN-1));
-	std::memcpy(&model_input[StartFeatures + FEATURE_LEN*(HISTORY_BUFFER_LEN-1)], &model_raw_preds[OUTPUT_SIZE], sizeof(float) * FEATURE_LEN);
+		// handle features
+		std::memmove(&model_input[StartFeatures], &model_input[StartFeatures + FEATURE_LEN], sizeof(float) * FEATURE_LEN*(HISTORY_BUFFER_LEN-1));
+		std::memcpy(&model_input[StartFeatures + FEATURE_LEN*(HISTORY_BUFFER_LEN-1)], &model_raw_preds[OUTPUT_SIZE], sizeof(float) * FEATURE_LEN);
 
         cout << "Server generated output float array" << endl;
 
         // write the data to the output stream without writing the length first
-        //success = writeFully(client_sock, (char*)model_raw_preds, NET_OUTPUT_SIZE);
+        success = writeFully(client_sock, (char*)model_raw_preds, NET_OUTPUT_SIZE * 4);
 
-        //if (!success) { // if an error occurred or end of stream reached
-        //    break; // break from the loop
-        //}
+        if (!success) { // if an error occurred or end of stream reached
+            client_sock = -1;
+			sched_yield();
+			continue;
+        }
 
-        //cout << "Client sent data to another C++ application" << endl;
+        cout << "Client sent data to another C++ application" << endl;
 
         // no need to free the memory allocated for the buffer and the float array inside the loop
     }
