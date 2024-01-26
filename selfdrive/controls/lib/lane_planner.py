8874ecec1927a12c04ffa44aa9cf036533f28012
Phr00t <phr00t@gmail.com>
2024-01-26 16:55:17 -0500

    little more accurate lane width calculation for lane centering fadeoff

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -192,7 +192,7 @@ class LanePlanner:
       # if we are lane changing, cut center force
       self.center_force *= self.lane_change_multiplier
       # if we are in a small lane, reduce centering force to prevent pingponging
-      self.center_force *= interp(self.lane_width, [2.6, 2.8], [0.0, 1.0])
+      self.center_force *= interp((lane_tightness + self.lane_width) * 0.5, [2.6, 2.8], [0.0, 1.0])
       # apply a cap centering force
       self.center_force = clamp(self.center_force, -0.8, 0.8)
       # apply less lane centering for a direction we are already turning
