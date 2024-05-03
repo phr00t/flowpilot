57d7d230caace11725a2c2e5eabd73530ba165c4
jorishermus <42472712+jorishermus@users.noreply.github.com>
2024-05-03 18:49:32 +0200

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -619,7 +619,7 @@ class Controls:
     if not self.read_only and self.initialized:
       # send car controls over can
       now_nanos = self.can_log_mono_time if REPLAY else int(sec_since_boot() * 1e9)
-      self.last_actuators, can_sends = self.CI.apply(self.VM, CC, self.sm, self.v_cruise_helper.v_cruise_kph, now_nanos)
+      self.last_actuators, can_sends = self.CI.apply(CC)
       self.pm.send('sendcan', can_list_to_can_capnp(can_sends, msgtype='sendcan', valid=CS.canValid))
       CC.actuatorsOutput = self.last_actuators
       if self.CP.steerControlType == car.CarParams.SteerControlType.angle:
