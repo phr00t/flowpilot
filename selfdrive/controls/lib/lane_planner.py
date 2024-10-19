265e42242874b44e85031223c4cc1bd6492069e7
Phr00t <phr00t@gmail.com>
2024-10-19 18:36:18 -0400

    steer a little more right tuning

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -18,7 +18,7 @@ TYPICAL_MIN_LANE_DISTANCE = 2.7
 TYPICAL_MAX_LANE_DISTANCE = 3.4
 CENTER_FORCE_GENERAL_SCALE = 0.48
 # higher offset means steering more right
-DESIRED_CURVE_OFFSET = 0.048
+DESIRED_CURVE_OFFSET = 0.052
 DESIRED_CURVE_TO_STEERANGLE_RATIO = -0.04
 # overall desire curve steer scale, set to 0 to disable using desired_curvature
 STEER_DISAGREEMENT_SCALE = 0.068
